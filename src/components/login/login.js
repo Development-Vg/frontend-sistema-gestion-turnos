ce2dc075552493b1fdfa0f893238392eb37b674b
Anderseon200 <anderson.barreto01@uptc.edu.co>
2024-05-03 16:22:26 -0500

    structure

---
+++ src/components/login/login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Aquí puedes manejar la lógica de autenticación
+        console.log(`Username: ${username}, Password: ${password}`);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Nombre de usuario:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Contraseña:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <input type="submit" value="Iniciar sesión" />
+        </form>
+    );
+}
+
+export default Login;
---
+++ src/router/router.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Login from "../components/login/login";
+import Registry from '../components/registry/registry';
+
+function AppRouter() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/registry" element={<Registry />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
