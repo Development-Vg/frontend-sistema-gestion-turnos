aaf3acb6104e31ad0d0e43810aacd0fc564871f8
SergioSoto19 <ser192000gio@gmail.com>
2024-05-25 22:52:47 -0500

    actualizaicon de homeprincipal

--- src/components/HomeSG/Homec/Homec.js
+++ src/components/HomeSG/Homec/Homec.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Homec.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Row, Col } from 'react-bootstrap';
 
 export const Homec = () => {
     return (
@@ -8,18 +9,30 @@ export const Homec = () => {
             <section className="homec">
                 <div className="container">
                     <div className="row">
-                        <h1>¡Bienvenido a la Plataforma de Solicitud de Turnos de la Clínica SDGT!</h1>
-                        <p>Nos complace darle la bienvenida a nuestra plataforma diseñada para facilitar la gestión de sus citas médicas.
-                         Aquí podrá:</p>
+
+                        <div>
+                            <Row>
+                            <Col xs={6}>
+                                    <h1>¡Bienvenido a la Plataforma de Solicitud de Turnos de la Clínica SDGT!</h1>
+                                    <p>Nos complace darle la bienvenida a nuestra plataforma diseñada para facilitar la gestión de sus citas médicas.
+                                        Aquí podrá:</p>
+                                </Col>
+                                
+                            </Row>
+
+                        </div>
+
+
                         <ul className="option">
-                            <li><FontAwesomeIcon icon="fa-solid fa-magnifying-glass" style={{ marginRight: '12px' }}/>Consultar la disponibilidad de turnos</li>
-                            <li><FontAwesomeIcon icon="fa-solid fa-stethoscope" style={{ marginRight: '10px' }}/>Solicitar turnos para consultas médicas</li>
-                            <li><FontAwesomeIcon icon="fa-solid fa-clipboard" style={{ marginRight: '15px' }}/>Consultar el historial de sus turnos</li>
+                            <li><FontAwesomeIcon icon="fa-solid fa-magnifying-glass" style={{ marginRight: '12px' }} />Consultar la disponibilidad de turnos</li>
+                            <li><FontAwesomeIcon icon="fa-solid fa-stethoscope" style={{ marginRight: '10px' }} />Solicitar turnos para consultas médicas</li>
+                            <li><FontAwesomeIcon icon="fa-solid fa-clipboard" style={{ marginRight: '15px' }} />Consultar el historial de sus turnos</li>
                         </ul>
-                         <div className="button">
-                            <button className="primary-btn">solicitar turno</button>
-                            <button  className="primary-btn">registrarse</button>
-                         </div>
+                        <div className="button">
+
+                            <button className="primary-btn m-2">solicitar turno</button>
+                            <button className="primary-btn">registrarse</button>
+                        </div>
                     </div>
                 </div>
 
